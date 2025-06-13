5ccf20368590b204e035e889151f70fe8fc3e5eb
Jodexx <0984134q@gmail.com>
2025-06-13 22:04:50 +0200

    AdvancedGui test

--- plugin/src/main/java/com/jodexindustries/jguiwrapper/plugin/JGuiPlugin.java
+++ plugin/src/main/java/com/jodexindustries/jguiwrapper/plugin/JGuiPlugin.java
@@ -1,6 +1,11 @@
 package com.jodexindustries.jguiwrapper.plugin;
 
+import com.jodexindustries.jguiwrapper.api.item.ItemWrapper;
 import com.jodexindustries.jguiwrapper.common.JGuiInitializer;
+import com.jodexindustries.jguiwrapper.gui.advanced.AdvancedGui;
+import com.jodexindustries.jguiwrapper.gui.advanced.GuiItemController;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,7 +16,9 @@ import java.util.Objects;
 
 public final class JGuiPlugin extends JavaPlugin {
 
-    private static final TestGui GUI = new TestGui();
+    private static final TestGui SIMPLE_GUI = new TestGui();
+
+    private static final AdvancedGui ADVANCED_GUI = new AdvancedGui("&cAdvanced gui");
 
     @Override
     public void onEnable() {
@@ -23,7 +30,18 @@ public final class JGuiPlugin extends JavaPlugin {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
         if (!(sender instanceof Player player)) return false;
 
-        GUI.open(player);
+//        SIMPLE_GUI.open(player);
+
+        ADVANCED_GUI.registerItem("test", ItemWrapper.builder(Material.GOLD_BLOCK).build(), 0, (event) -> {
+            event.setCancelled(true);
+            GuiItemController controller = ADVANCED_GUI.getController(event.getRawSlot());
+            controller.updateItem(itemWrapper -> {
+                itemWrapper.displayName(Component.text(Math.random()));
+                itemWrapper.update();
+            });
+        });
+
+        ADVANCED_GUI.open(player);
 
         return true;
     }
