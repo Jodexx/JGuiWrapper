fdcfaf08e158d1ff576e61798a7f08141295caa3
Jodexx <0984134q@gmail.com>
2025-07-13 15:02:58 +0200

    NMS 1.19

---
+++ nms/v1_19_R1/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    paperweight.paperDevBundle("1.19-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
---
+++ nms/v1_19_R3/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    paperweight.paperDevBundle("1.19.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
