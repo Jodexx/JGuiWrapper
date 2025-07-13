82fc83cf8580ed897ce2039359cd14dee36333ef
Jodexx <0984134q@gmail.com>
2025-07-13 18:25:14 +0200

    1.18-1.18.2 NMS support

---
+++ nms/v1_18_R1/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    paperweight.paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
---
+++ nms/v1_18_R2/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    paperweight.paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
