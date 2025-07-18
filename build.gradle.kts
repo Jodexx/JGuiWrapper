fe6d5a6e9e057f4cf1f1a80e56d49bae9bdf651d
Jodexx <0984134q@gmail.com>
2025-07-18 18:58:11 +0200

    back to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ subprojects {
 
 allprojects {
     group = "com.jodexindustries.jguiwrapper"
-    version = "1.0.1"
+    version = "1.0.0"
 
     repositories {
         maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
