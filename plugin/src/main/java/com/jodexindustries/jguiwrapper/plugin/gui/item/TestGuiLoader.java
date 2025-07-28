903be33e6e9f4fa9322994cde41e39d6d561537d
Jodexx <0984134q@gmail.com>
2025-07-28 20:39:59 +0200

    enhance TestGuiLoader and TestItemHandler to support placeholder registration and click tracking

--- plugin/src/main/java/com/jodexindustries/jguiwrapper/plugin/gui/item/TestGuiLoader.java
+++ plugin/src/main/java/com/jodexindustries/jguiwrapper/plugin/gui/item/TestGuiLoader.java
@@ -1,16 +1,45 @@
 package com.jodexindustries.jguiwrapper.plugin.gui.item;
 
 import com.jodexindustries.jguiwrapper.api.gui.GuiDataLoader;
+import com.jodexindustries.jguiwrapper.api.placeholder.PlaceholderEngine;
+import com.jodexindustries.jguiwrapper.gui.advanced.AdvancedGui;
+import com.jodexindustries.jguiwrapper.gui.advanced.GuiItemController;
 import org.bukkit.entity.HumanEntity;
 
 @SuppressWarnings("unused")
 public class TestGuiLoader implements GuiDataLoader {
 
+    private final PlaceholderEngine placeholderEngine = PlaceholderEngine.of();
+
     private int openCount;
+    private int clickCount;
+
+    public TestGuiLoader() {
+        placeholderEngine.register("%open_count%", (player) -> String.valueOf(openCount));
+        placeholderEngine.register("%click_count%", (player) -> {
+            if (clickCount >= 100) return ">= 100";
+
+            return String.valueOf(clickCount);
+        });
+    }
 
     @Override
-    public void load(HumanEntity player) {
+    public void load(AdvancedGui gui, HumanEntity player) {
         openCount++;
+
+        for (GuiItemController controller : gui.getControllers()) {
+            controller.updateItems(itemWrapper -> {
+                if (itemWrapper.placeholderEngine() == null) {
+                    itemWrapper.placeholderEngine(placeholderEngine);
+                } else {
+                    itemWrapper.placeholderEngine().addAll(placeholderEngine);
+                }
+            });
+        }
+    }
+
+    public void click() {
+        clickCount++;
     }
 
     public int getOpenCount() {
--- plugin/src/main/java/com/jodexindustries/jguiwrapper/plugin/gui/item/TestItemHandler.java
+++ plugin/src/main/java/com/jodexindustries/jguiwrapper/plugin/gui/item/TestItemHandler.java
@@ -3,6 +3,7 @@ package com.jodexindustries.jguiwrapper.plugin.gui.item;
 import com.jodexindustries.jguiwrapper.api.gui.handler.item.HandlerContext;
 import com.jodexindustries.jguiwrapper.api.gui.handler.item.ItemHandler;
 import com.jodexindustries.jguiwrapper.gui.advanced.GuiItemController;
+import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
@@ -10,6 +11,13 @@ public class TestItemHandler implements ItemHandler<TestGuiLoader> {
 
     @Override
     public void load(@NotNull TestGuiLoader loader, @NotNull GuiItemController controller, @NotNull HandlerContext context) {
-        controller.updateItems(itemWrapper -> itemWrapper.displayName("Open count: " + loader.getOpenCount()));
+        controller.defaultClickHandler((e, c) -> {
+            e.setCancelled(true);
+            loader.click();
+            c.updateItems(itemWrapper -> itemWrapper.update(e.getWhoClicked()));
+        });
+
+        controller.updateItems(wrapper -> wrapper.lore("&cPlayer: &6%player_name%", "&cOpen count: &6%open_count%", "&cClick count: &6%click_count%"),
+                (OfflinePlayer) context.player());
     }
 }
