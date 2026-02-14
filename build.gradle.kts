ad07bbb4c5ea4b0301e80978e54846b34f2b5700
Jodexx <0984134q@gmail.com>
2026-02-14 20:40:17 +0100

    update gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     java
     `java-library`
     `maven-publish`
-    id("com.gradleup.shadow") version "9.0.2" apply false
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18" apply false
+    id("com.gradleup.shadow") version "9.3.1" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" apply false
 }
 
 tasks.jar {
