2619d5c33ec3bd288b92b6651d22b333ce773193
Jodexx <0984134q@gmail.com>
2026-02-17 22:28:55 +0100

    remove unused nms dependency

--- paper/paper-common/build.gradle.kts
+++ paper/paper-common/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
     api(project(":common"))
     api(project(":paper:paper-api"))
-    compileOnly(project(":paper:nms"))
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
 }
